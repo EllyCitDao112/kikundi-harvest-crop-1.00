255984d77a0abed0bc0cf413a3ecf70ac23a762f
ellycit.international.dao.1.10@gmail.com <131910208+EllyCitDao112@users.noreply.github.com>
2025-07-09 20:05:21 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/kikundi-harvest-crop-1.00/',
   plugins: [react()],
-  build: {
-    outDir: 'docs'
-  }
-});
+  build:{outDir:'docs'}});
